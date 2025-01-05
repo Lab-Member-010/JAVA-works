9b3f55f6cb4f637113d74d50d86fb257869c0dd1
Lab-Member-010 <jayeshsharmalab@gmail.com>
2025-01-05 23:00:25 +0530

    Mathemetical logics

---
+++ Mathematical/Assignment1.java
@@ -0,0 +1,411 @@
+class Assignment1{
+	public static void main(String args[]){
+		//Q.1
+		//double p=230;
+		//double l=70;
+		//double b=(p/2)-l;
+		//System.out.println("Bredth: "+b);
+		//double a=l*b;
+		//System.out.println("Area: "+a);
+		//Output=45,3150
+
+
+		//Q.2
+		//double a=96;
+		//double b=8;
+		//double l=a/b;
+		//double p=2*(l+b);
+		//System.out.println("Length and perimeter: "+l+", "+p);
+		//Output=12, 40
+
+		//Q.3
+		//double l1=520;
+		//double b1=140;
+		//double a1=l1*b1;
+		//double l2=13;
+		//double b2=7;
+		//double a2=l2*b2;
+		//double req=a1/a2;
+		//System.out.println("Required Tiles: "+req);
+		//Output= 800
+
+		//Q.4
+		//double l=300;
+		//double b=150;
+		//double a=l*b;
+		//double cost=(a*6)/100;
+		//System.out.println("Total cost: "+cost);
+		//Output= 2700
+
+		//Q.5
+		//double total=1600;
+		//double per_sq_m=25;
+		//double l=20;
+		//double a=total/per_sq_m;
+		//double b=a/l;
+		//double p=2*(l+b);
+		//System.out.println("Perimeter & Bredth: "+p+", "+b);
+		//Output= 46.4, 3.2
+
+		//Q.6
+		//double s1=10;
+		//double s2=9;
+		//double p=36;
+		//double s3=p-(s1+s2);
+		//double s=p/2;
+		//double a=Math.sqrt(s*(s-s1)*(s-s2)*(s-s3));
+		//System.out.println("Area: "+a);
+		//Output= 36
+
+		//Q.7
+		//double a=500;
+		//double b=50;
+		//double h=(a*2)/b;
+		//System.out.println("Height:"+h);
+		//Output=20
+
+		//Q.8
+		//double a=80;
+		//double h=20;
+		//double b=(a*2)/h;
+		//System.out.println("Bredth: "+b);
+		//output= 8
+
+		//Q.9
+		//double l=12;
+		//double a=0.43*l*l;
+		//System.out.println("Area; "+a);
+		//Output= 61.92
+
+		//Q.10
+		//double h=15;
+		//double b=15;
+		//double a=(h*b)/2;
+		//System.out.println("Area: "+a);		
+		//Output= 112.5
+
+		//Q.11
+		//double a=320;
+		//double x=Math.sqrt((a*2)/40);
+		//double h=8*x;
+		//double b=5*x;
+		//System.out.println("Height & Bredth: "+h+", "+b);
+		//Output= 32, 20
+
+		//Q.12
+		//double h=13;
+		//double l1=12;
+		//double l2=Math.sqrt((h*h)-(l1*l1));
+		//System.out.println("Other Length: "+l2);
+		//double a=(l1*l2)/2;
+		//System.out.println("Area; "+a);
+		//Output= 5, 30
+
+		//Q.13
+		//double a=184;
+		//double l1=16;
+		//double l2=(a*2)/l1;
+		//System.out.println("Other Length; "+l2);
+		//Output= 23
+
+		//Q.14
+		// double d=46;
+		// double h1=13;
+		// double h2=10;
+		// double a=(d*(h1+h2))/2;
+		//System.out.println("Area: "+a);
+		//Output= 529
+
+		//Q.15
+		// double l=22;
+		// double b=15;
+		// double a1=l*b;
+		// double s=21;
+		// double a2=s*s;
+		// if(a1>a2)
+		// 	System.out.println("Shelly's garden is greater");
+		// else if(a2>a1)
+		// 	System.out.println("Rachel's garden is greater");
+		// else
+		// 	System.out.println("Both equal");
+		//Output= Rachel's garden is greater
+
+		//Q.16
+		// double l=5;
+		// double b=7;
+		// double a=l*b;
+		// System.out.println("Required carpet: "+a);
+		//Output= 35
+
+		//Q.17
+		// double l=3;
+		// double b=2;
+		// double p=2*(l+b);
+		// System.out.println("Required Ribbon: "+p);
+		//Output= 10
+
+		//Q.18
+		// double l=50;
+		// double b=30;
+		// double p=2*(l+b);
+		// System.out.println("Total Running: "+(p*10));
+		//Output= 1600
+
+		//Q.19
+		// double e=7;
+		// double v1=e*e*e;
+		// double l=7;
+		// double b=4;
+		// double h=8;
+		// double v2=l*b*h;
+		// if (v2>v1) {
+		// 	System.out.println("Cuboid is greater");
+		// }else if (v1>v2) {
+		// 	System.out.println("Cube is greater");
+		// }else{
+		// 	System.out.println("both equal");
+		// }
+		//Output= Cube is greater
+		
+		//Q.20
+		// double l=25;
+		// double b=10;
+		// double h=8;
+		// double v=l*b*h;
+		// System.out.println("Volume: "+v);
+		//Output= 2000
+
+		//Q.21
+		//double l1=15;
+		//double b1=8;
+		//double h1=5;
+		//double v1=l1*b1*h1;
+		//double l2=15*100;
+		//double b2=10*100;
+		//double h2=8*100;
+		//double v2=l2*b2*h2;
+		//double comp=v2/v1;
+		//System.out.println("Bricks needed: "+comp);
+		//Output=2000000
+		
+		//Q.22
+		// double l=150;
+		// double b=30;
+		// double h=2;
+		// double v=l*b*h;
+		// System.out.println("Capacity: "+v);
+		//Output= 9000
+
+		//Q.23
+		// double e=3;
+		// double v1=e*e*e;
+		// double l=15;
+		// double b=9;
+		// double h=12;
+		// double v2=l*b*h;
+		// System.out.println("Cubical boxes required: "+(v2/v1));
+		//Output= 60
+
+		//Q.24
+		// double l1=25;
+		// double b1=10;
+		// double h1=7.5;
+		// double v1=l1*b1*h1;
+		// double l2=2*100;
+		// double b2=0.75*100;
+		// double h2=2*100;
+		// double v2=l2*b2*h2;
+		// System.out.println("Total Price: "+(((v2/v1)*900)/100));
+		//Output= 14400
+
+		//Q.25
+		// double l=24;
+		// double b=15;
+		// double a=l*b;
+		// System.out.println("Area of path: "+(a*100));
+		//Output= 36000
+
+		//Q.26
+		// double l1=120*100;
+		// double b1=2.4*100;
+		// double a1=l1*b1;
+		// double l2=24;
+		// double b2=15;
+		// double a2=l2*b2;
+		// System.out.println("Bricks Needed: "+(a1/a2));
+		//Output= 8000
+
+		//Q.27
+		// double l=20;
+		// double b=15;
+		// double a=l*b;
+		// System.out.println("Total cost: "+(a*5));
+		//Output= 1500
+
+		//Q.28
+		// double l=5;
+		// double b=4;
+		// double a=l*b;
+		// System.out.println("Total cost: "+(a*205));
+		//Output= 4100
+
+		//Q.29
+		// double l=800;
+		// double b=900;
+		// double a1=l*b;
+		// double s=10;
+		// double a2=s*s;
+		// System.out.println("Total tiles needed: "+(a1/a2));
+		//Output= 7200
+
+		//Q.30
+		// double l1=200;
+		// double b1=400;
+		// double a1=l1*b1;
+		// double l2=5;
+		// double b2=8;
+		// double a2=l2*b2;
+		// System.out.println("Total Tile: "+(a1/a2));
+		//Output= 2000
+
+		//Q.31
+		// double p=200;
+		// double s=p/4;
+		// double a=s*s;
+		// System.out.println("Area: "+a);
+		//Output= 2500
+
+		//Q.32
+		// double s1=150;
+		// double a1=s1*s1;
+		// double s2=25;
+		// double a2=s2*s2;
+		// double a=a1-a2;
+		// System.out.println("Area: "+a);
+		//Output= 21875
+
+		//Q.33
+		// double l=30;
+		// double b=20;
+		// double a=l*b;
+		// double l1=20;
+		// double b1=4;
+		// double a1=l1*b1;
+		// double l2=30;
+		// double b2=3;
+		// double a2=l2*b2;
+		// double l3=3;
+		// double b3=4;
+		// double a3=l3*b3;
+		// double area=a-((a1+a2)-a3);
+		// System.out.println("Area: "+area);
+		//Output= 442
+
+		//Q.34
+		// double b1=92;
+		// double b2=128;
+		// double h=40;
+		// double a1=((b1+b2)*h)/2;
+		// double b=4;
+		// double a2=h*b;
+		// double a=a1-a2;
+		// System.out.println("Area: "+a);
+		//Output= 4240
+
+		//Q.35
+		// double r=7.7;
+		// double h=12;
+		// double pi=3.14;
+		// double a=2*pi*r*(r+h);
+		// System.out.println("Area: "+a);
+		//Output= 952.09638
+
+		//Q.36
+		// double d=15;
+		// double r=d/2;
+		// double h=7;
+		// double pi=3.14;
+		// double a=2*pi*r*(r+h);
+		// System.out.println("Area: "+a);
+		//Ouput= 682.95
+
+		//Q.37
+		// double a=149;
+		// double h=6;
+		// double pi=3.14;
+		// double d=2*(Math.sqrt((a/(2*pi))+(h/2)*(h/2)))-(h/2);
+		// System.out.println("Diameter: "+d);
+		//Output= 8.44134863548551
+
+		//Q.38
+		// double v=1287;
+		// double pi=3.14;
+		// double r=10;
+		// double h=v/(pi*r*r);
+		// double a=2*pi*r*(r+h);
+		// System.out.println("Area: "+a);
+		//Output= 885.4000000000001
+
+		// Q.39
+		// double d=12;
+		// double r=d/2;
+		// double h=9;
+		// double pi=3.14;
+		// double a=2*pi*r*(r+h);
+		// System.out.println("Area: "+a);
+		//Output= 565.2
+
+		//Q.40
+		// double pi=3.14;
+		// double r=5;
+		// double h=9;
+		// double v=pi*r*r*h;
+		// System.out.println("Volume: "+v);
+		//Output= 706.5
+
+		//Q.41 
+		// double pi=3.14;
+		// double h=50;
+		// double d=30;
+		// double r=d/2;
+		// double v=pi*r*r*h;
+		// System.out.println("Volume: "+v);
+		//Output= 35325
+		
+		//Q.42
+		// double pi= 3.14;
+		// double d=2.25;
+		// double r=d/2;
+		// double h=2.25;
+		// double v=pi*r*r*h;
+		// System.out.println("Volume: "+v);
+		//Output= 8.941640625
+
+		//Q.43
+		// double pi=3.14;
+		// double h=4;
+		// double s=5;
+		// double r=Math.sqrt((s*s)-(h*h));
+		// double a=pi*r;
+		// System.out.println("Cost: "+(a*10));
+		//Output= 94.2
+
+		//Q.44
+		// double a=-21;
+		// double b=-18;
+		// double d=b-a;
+		// double n=28;
+		// double t28=a+((n-1)*d);
+		// System.out.println("28th term: "+t28);
+		//Output= 60
+
+		//Q.45
+		double a=-21;
+		double b=-18;
+		double d=b-a;
+		double n=28;
+		double s=(n*(2*a+((n-1)*d)))/2;
+		System.out.println("Sum of 28 terms: "+s);
+		//Output= 546
+	}
+}
